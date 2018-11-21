001aea7c78bd5df57761274617c984c7e36f3405
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2018-11-21 17:26:43 +1100

    fix write feather

--- gdmtables/dynowindow/R/dynowindow.R
+++ gdmtables/dynowindow/R/dynowindow.R
@@ -38,7 +38,7 @@ gen_windows = function(pairs, variables, mstat, cstat, window,
     
   pyfile = paste(.libPaths(), 'dynowindow/exec/pyper.py', sep = '/')
   
-  call = sprintf('%s %s -f %s -s %s -m %s -e %s -w %s -src %s', 
+  call = sprintf('%s %s -f %s -s %s -m %s -e %s -w %s', 
                  exe, pyfile, pairs_dst, mstat, cstat, variables, window)
   
   if(!is.null(npy_src)){
