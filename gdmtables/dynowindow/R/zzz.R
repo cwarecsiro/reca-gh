1af3e90b880e9891298ed71da2ae7e82ecfc5fdf
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2018-11-22 13:14:37 +1100

    fixes

--- gdmtables/dynowindow/R/zzz.R
+++ gdmtables/dynowindow/R/zzz.R
@@ -2,7 +2,7 @@
   
   this_env = unname(Sys.info()['sysname'])
   
-  if(this_env == 'windows'){
+  if(this_env == 'Windows'){
     
     pth = unname(Sys.which('python'))
     if (pth == ''){
@@ -14,12 +14,18 @@
       pth = c(pth, conda)
     }
     
+    sys_conda = 'C:\\ProgramData\\Anaconda3\\python.exe'
+    if(file.exists(sys_conda)){
+      pth = c(pth, sys_conda)
+    }
+    
     if(is.null(pth)){
       warning(sprintf('%s %s', 
                       'Cannot locate a version of python.', 
                       'A variable exe needs to be available pointing to a python .exe'))
     } else {
       
+      # probably should choose among these sensible (by date?) Choose opt 1 for now.
       exe <<- pth[1]
       
     }
@@ -27,6 +33,8 @@
   } else {
     
     # assume linux - this good enough?
+    # Should test this? 
+    # vers = system(python -c 'import sys; print sys.version_info', intern = TRUE)
     
     exe <<- 'python3'
     
