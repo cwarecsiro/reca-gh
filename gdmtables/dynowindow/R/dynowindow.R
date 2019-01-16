250e507b8eea9ded9581fea6fcdc4e43d2a1d443
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2019-01-16 16:17:54 +1100

    fixed error in error checks...;

--- gdmtables/dynowindow/R/dynowindow.R
+++ gdmtables/dynowindow/R/dynowindow.R
@@ -11,7 +11,6 @@
 #' 
 #'@importFrom feather write_feather read_feather
 #'@export
-#'
 gen_windows = function(pairs, variables, mstat, cstat, window, 
                        pairs_dst = NULL, npy_src = NULL, start_year = 1911){
   
@@ -23,31 +22,34 @@ gen_windows = function(pairs, variables, mstat, cstat, window,
   
   type_pairs = class(pairs)
   if(type_pairs != 'character'){
-  
+    
     # data.frame like
-  
+    
     if(is.null(pairs_dst)){
       pairs_dst = tempfile(fileext = '.feather')
     }  
     
-    pairs_class = all(sapply(df, class) == 'numeric')
-    if(!pairs_class)  pairs = sapply(df, as.numeric)
+    col_class = rep(c('numeric', 'numeric', 'integer', 'integer'), 2)
+    for (c in 1:8){
+      class(pairs[, i]) = col_class[i]
+    }
     
-    min_year = c(pairs[, 3], pairs[, 7])
+    min_year = min(c(pairs[, 3], pairs[, 7]))
     if ((min_year - window) < start_year){
       
       # 
       stop(sprintf('Found year: %s. Cannot build climate windows for period before %s', 
-                      min_year, start_year))
+                   min_year, start_year))
       
     }
+    
     write_feather(pairs, pairs_dst)
     
   } else {
     
     pairs_dst = pairs
   }
-    
+  
   pyfile = paste(.libPaths(), 'dynowindow/exec/pyper.py', sep = '/')
   if(Sys.info()['sysname'] == 'Windows'){
     pyfile = gsub('/', '\\\\', pyfile)
@@ -73,7 +75,7 @@ gen_windows = function(pairs, variables, mstat, cstat, window,
   
   if(!length(grep('data.frame', class(output)))){
     stop(sprintf('Could not read output file. Error: %s', output_fp))
-  
+    
   } else {
     
     return(as.data.frame(output))
